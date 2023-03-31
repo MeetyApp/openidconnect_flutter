3fb00548eee83dc9501d1da49e80e98b9e77422b
WladimirHSJW <87383150+WowaHSJW@users.noreply.github.com>
2023-04-01 01:53:36 +0200

    Update android_ios.dart

--- openidconnect/lib/src/android_ios.dart
+++ openidconnect/lib/src/android_ios.dart
@@ -15,32 +15,36 @@ class OpenIdConnectAndroidiOS {
       context: context,
       barrierDismissible: false,
       builder: (dialogContext) {
-        return AlertDialog(
-          contentPadding: EdgeInsets.all(0),
-          insetPadding: EdgeInsets.all(0),
-          actions: [
-            IconButton(
-              onPressed: () => Navigator.pop(dialogContext, null),
-              icon: Icon(Icons.close),
+        return Stack(
+          children: [
+            AlertDialog(
+              contentPadding: EdgeInsets.all(0),
+              content: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: flutterWebView.WebView(
+                  javascriptMode: flutterWebView.JavascriptMode.unrestricted,
+                  initialUrl: authorizationUrl,
+                  onPageFinished: (url) {
+                    if (url.startsWith(redirectUrl)) {
+                      Navigator.pop(dialogContext, url);
+                    }
+                  },
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () => Navigator.pop(dialogContext, null),
+                icon: Icon(Icons.close),
+              ),
             ),
           ],
-          content: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: flutterWebView.WebView(
-              javascriptMode: flutterWebView.JavascriptMode.unrestricted,
-              initialUrl: authorizationUrl,
-              onPageFinished: (url) {
-                if (url.startsWith(redirectUrl)) {
-                  Navigator.pop(dialogContext, url);
-                }
-              },
-            ),
-          ),
-          title: Text(title),
         );
       },
     );
+
 
     if (result == null) throw AuthenticationException(ERROR_USER_CLOSED);
 
